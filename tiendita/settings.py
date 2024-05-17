79289090cadad7788f47cd53c13689a52ec9ff2a
Angel <ang.rojasc@duocuc.cl>
2024-05-17 01:40:17 -0400

    Se cambia DB

--- tiendita/settings.py
+++ tiendita/settings.py
@@ -91,16 +91,16 @@ WSGI_APPLICATION = 'tiendita.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 """
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -111,7 +111,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 
 
 
